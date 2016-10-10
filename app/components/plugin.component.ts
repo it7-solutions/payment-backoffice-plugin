6a3fbc2759f7c5b45f18ffd44910e5cd48f26030
vherever <vherever@gmail.com>
2016-10-10 15:50:10 +0300

    basic setup, webpack start, build, minify

---
+++ app/components/plugin.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/templates/plugin.component.html'
+})
+export class AppComponent {
+    constructor() {}
+}
