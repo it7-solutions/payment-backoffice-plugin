8ca380f42ba037486dc95d4ede847d72efb436bd
vherever <vherever@gmail.com>
2016-10-13 13:00:05 +0300

    show selected values

--- app/components/view-type.component.ts
+++ app/components/view-type.component.ts
@@ -1,9 +1,36 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {PluginConfig} from "../services/plugin.config";
+import {TypeSelect} from "../models/type-select";
+import {DynamicFlags} from "../services/dynamic-flags.service";
 @Component({
     selector: 'view-type',
     templateUrl: PluginConfig.buildTemplateUrl('/templates/view-type.component.html')
 })
 export class ViewTypeComponent {
+    @Input() selected: TypeSelect;
+    chosen_payment_type: string;
 
+    constructor(
+        private config: PluginConfig,
+        private dynamicFlags: DynamicFlags
+    ) {}
+
+    getPaymentLabelByKey() {
+        var key = this.dynamicFlags.chosen_payment_type;
+        return this.config.payment_types.find((d) => {
+            return d.key === key;
+        }).label;
+    }
+    getPriceLabelByKey() {
+        var key = this.dynamicFlags.chosen_price_type;
+        return this.config.price_types.find((d) => {
+            return d.key === key;
+        }).label;
+    }
+    getRegServiceNameById() {
+        var key = this.dynamicFlags.chosen_reg_service_id;
+        return this.config.reg_services_out.find((d) => {
+            return d.id === key;
+        }).name;
+    }
 }
--- app/services/dynamic-flags.service.ts
+++ app/services/dynamic-flags.service.ts
@@ -4,6 +4,9 @@ import {BehaviorSubject, Observable} from "rxjs/Rx";
 @Injectable()
 export class DynamicFlags {
     show_form: boolean = false;
+    chosen_payment_type: string;
+    chosen_price_type: string;
+    chosen_reg_service_id: number;
 
     private _onUpdate: BehaviorSubject<DynamicFlags>;
     public onUpdate: Observable<DynamicFlags>;
@@ -16,6 +19,9 @@ export class DynamicFlags {
 
     public update(options: any) {
         undefined === options.show_form || (this.show_form = options.show_form);
+        undefined === options.chosen_payment_type || (this.chosen_payment_type = options.chosen_payment_type);
+        undefined === options.chosen_price_type || (this.chosen_price_type = options.chosen_price_type);
+        undefined === options.chosen_reg_service_id || (this.chosen_reg_service_id = options.chosen_reg_service_id);
 
         this._onUpdate.next(this);
     }
