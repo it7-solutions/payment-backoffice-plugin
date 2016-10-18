d5f19c345d03026161aa7180adeb0f86097fc2b2
vherever <vherever@gmail.com>
2016-10-18 16:20:31 +0300

    add form validation

--- app/components/add-form.component.ts
+++ app/components/add-form.component.ts
@@ -3,6 +3,7 @@ import {AddForm} from "../models/addForm";
 import {DataManagerService} from "../services/data-manager.service";
 import {PluginConfig} from "../services/plugin.config";
 import {DynamicFlags} from "../services/dynamic-flags.service";
+import {ValidateField} from "../models/validate";
 @Component({
     selector: 'add-form',
     templateUrl: PluginConfig.buildTemplateUrl('/templates/add-form.component.html')
@@ -25,21 +26,27 @@ export class AddFormComponent{
     };
 
     onAddTransactionClick() {
-        this.dataManager.addTransactionRequest(this.addForm)
-            .then(
-                data => {
-                    this.dynamicFlags.update(data);
-                    this.addForm = {
-                        amount: '',
-                        invoice_id: '',
-                        payment_type_short_name: '',
-                        bank: '',
-                        transaction_date: ''
-                    };
-                    this._show_form = false;
-                    setTimeout(function(){ this._show_form = true; }.bind(this), 1);
-                }
-            );
+        this.onValidateFields();
+        console.log('form', this.formValid);
+        if(this.checkValid()) {
+            this.dataManager.addTransactionRequest(this.addForm)
+                .then(
+                    data => {
+                        this.dynamicFlags.update(data);
+                        this.addForm = {
+                            amount: '',
+                            invoice_id: '',
+                            payment_type_short_name: '',
+                            bank: '',
+                            transaction_date: ''
+                        };
+                        this._show_form = false;
+                        setTimeout(function () {
+                            this._show_form = true;
+                        }.bind(this), 1);
+                    }
+                );
+        }
     }
 
     ngAfterContentChecked() {
@@ -47,5 +54,75 @@ export class AddFormComponent{
         this.config.callDatePicker((date: string) => {
             this.addForm.transaction_date = date;
         });
+    }
+
+    formValid: boolean = true;
+    validateFields: {[key:string] : ValidateField} = {
+        amount: {
+            isValid: true,
+            messageText: '',
+            isRequired: true
+        },
+        invoice_id: {
+            isValid: true,
+            messageText: '',
+            isRequired: true,
+            isSelect: true
+        },
+        payment_type_short_name: {
+            isValid: true,
+            messageText: '',
+            isRequired: true,
+            isSelect: true
+        },
+        bank: {
+            isValid: true,
+            messageText: '',
+            isRequired: false
+        },
+        transaction_date: {
+            isValid: true,
+            messageText: '',
+            isRequired: false
+        }
+    };
+
+    private setAllValid() {
+        for(var fieldName in this.validateFields) {
+            var field:ValidateField = this.validateFields[fieldName];
+            field.isValid = true;
+            field.messageText = '';
+        }
+    }
+    private checkRequired() {
+        for(var fieldName in this.validateFields) {
+            var field:ValidateField = this.validateFields[fieldName];
+            if(field.isRequired) {
+                var value = this.addForm[fieldName];
+                if('' === value || value === null) {
+                    field.isValid = false;
+                    if(field.isSelect) {
+                        field.messageText = (field.messageText ? field.messageText + '. ' : '') + 'Please make selection!';
+                    } else {
+                        field.messageText = (field.messageText ? field.messageText + '. ' : '') + 'Please fill in the form!';
+                    }
+                }
+            }
+        }
+    }
+    private checkValid() {
+        this.formValid = true;
+        for(var i in this.validateFields) {
+            if(this.validateFields[i].isValid === false) {
+                this.formValid = false;
+                break;
+            }
+        }
+        return this.formValid;
+    }
+    onValidateFields() {
+        this.setAllValid();
+        this.checkRequired();
+        this.checkValid();
     }
 }
---
+++ app/models/validate.ts
@@ -0,0 +1,7 @@
+export interface ValidateField {
+    isValid: boolean,
+    messageText: string,
+    isRequired: boolean,
+    isSelect?: boolean
+}
+
